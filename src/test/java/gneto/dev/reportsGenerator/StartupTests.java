e0e9bc3853d2fc191a7ab032110252e6bb921cf2
Gilmar Santos Neto <Gilmar Santos>
2026-06-30 05:00:23 -0300

    starting project

---
+++ src/test/java/gneto/dev/reportsGenerator/StartupTests.java
@@ -0,0 +1,13 @@
+package gneto.dev.reportsGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
